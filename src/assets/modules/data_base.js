675ffab716be6a922558da5b7efc6821f4929737
yasinabdmahmood <abd181103@gmail.com>
2022-09-16 23:50:51 +0300

    Add salad meals array

--- src/assets/modules/data_base.js
+++ src/assets/modules/data_base.js
@@ -28,50 +28,6 @@
         order:0
     },
     {
-        src:'./assets/images/fishPancakes.jpg',
-        name:'Fish pancakes',
-        discription:'Beef, lettuce, pickle, tomato, soft bun',
-        price:6.5,
-        order:0
-    },
-    {
-        src:'./assets/images/hotRolls.jpg',
-        name:'Hot rolls',
-        discription:'Temporarily unavailable',
-        price:4.9,
-        order:0
-    },
- ]
- const ar2=[
-    {
-        src:'./assets/images/avocado.jpg',
-        name:'"Homemade" dumplings',
-        discription:'Vareniki with mashed potatoes and fried chanterelles',
-        price:4.5,
-        order:0
-    },
-    {
-        src:'./assets/images/soup.jpg',
-        name:'Soup',
-        discription:'Potatoes, carrots, onions, green onions',
-        price:4.3,
-        order:0
-    },
-    {
-        src:'./assets/images/burger.jpg',
-        name:'Burger',
-        discription:'Beef, lettuce, pickle, tomato, soft bun',
-        price:5.9,
-        order:0
-    },
-    {
-        src:'./assets/images/borscht.jpg',
-        name:'Borscht',
-        discription:'Beetroot borsch with bacon and donuts',
-        price:4.6,
-        order:0
-    },
-    {
         src:'./assets/images/fishPancake.jpg',
         name:'Fish pancakes',
         discription:'Beef, lettuce, pickle, tomato, soft bun',
@@ -85,6 +41,38 @@
         price:4.9,
         order:0
     },
+ ]
+ 
+ const ar2=[
+    {
+        src:'./assets/images/avogado.jpg',
+        name:'Avocado salad with feta',
+        discription:'Ripe avocado, feta cheese, olives and spinach salad',
+        price:4.9,
+        order:0
+    },
+    {
+        src:'./assets/images/camembert.jpg',
+        name:'Camembert salad',
+        discription:'Baked Camembert cheese with fresh vegetables',
+        price:4.5,
+        order:0
+    },
+    {
+        src:'./assets/images/greekSalad.jpg',
+        name:'Greek saladger',
+        discription:'Tomato, cucumber, olives, feta, olive oil',
+        price:5.5,
+        order:0
+    },
+    {
+        src:'./assets/images/Guacamole.jpg',
+        name:'Guacamole',
+        discription:'Mexican guacamole with nachos',
+        price:3.9,
+        order:0
+    },
+   
  ]
  const catagories=[
     {
