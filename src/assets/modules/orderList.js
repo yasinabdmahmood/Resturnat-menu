0c9ffdb6f799867bba2189c4ca79bf391425ac97
yasinabdmahmood <abd181103@gmail.com>
2022-09-17 16:05:26 +0300

    Implement order list funcionality

--- src/assets/modules/orderList.js
+++ src/assets/modules/orderList.js
@@ -7,22 +7,27 @@ const orderList={
     add:(obj)=>{
        const index = orderList.getIndex(obj);
        if(index<0){
-         const newObj={...obj} ;
+         const newObj=obj ;
          newObj.order=1
            orderList.list.push(newObj);
+           return 1
        }
        else{
            orderList.list[index].order+=1;
+           return  orderList.list[index].order
        }
+       
 
     },
     remove:(obj)=>{
        const index=orderList.getIndex(obj);
        if(orderList.list[index].order<=0){
            orderList.list.splice(index, 1);
+           return 0
        }
        else{
            orderList.list[index].order-=1;
+           return  orderList.list[index].order
        }
     }
 
--- src/index.js
+++ src/index.js
@@ -9,19 +9,33 @@ const plIcon=(e)=>{
     const card=e.target.parentNode.parentNode.parentNode;
     const cardInfo=catagories[parseInt(card.getAttribute('cat'))].list[parseInt(card.getAttribute('id2'))];
     console.log(cardInfo)
-    console.log(card)
+    console.log(orderList.list)
+    const order=orderList.add(cardInfo);
+    card.querySelector('.order-number').innerHTML=order
     card.querySelector('.pl').style.display='none';
     card.querySelector('.quantity').style.display='flex';
 }
 const plusIcon=(e)=>{
-    const card=e.target.parentNode.parentNode.parentNode.parentNode
-    console.log(card)
+    const card=e.target.parentNode.parentNode.parentNode.parentNode;
+    const cardInfo=catagories[parseInt(card.getAttribute('cat'))].list[parseInt(card.getAttribute('id2'))];
+    const order=orderList.add(cardInfo);
+    card.querySelector('.order-number').innerHTML=order
+    console.log(orderList.list)
+
 }
 const minusIcon=(e)=>{
     const card=e.target.parentNode.parentNode.parentNode.parentNode
-    console.log(card)
-    card.querySelector('.pl').style.display='block';
-    card.querySelector('.quantity').style.display='none';
+    console.log(orderList.list)
+    const cardInfo=catagories[parseInt(card.getAttribute('cat'))].list[parseInt(card.getAttribute('id2'))];
+    const order=orderList.remove(cardInfo);
+    if(order===0){
+        card.querySelector('.pl').style.display='block';
+        card.querySelector('.quantity').style.display='none';
+    }
+    else{
+        card.querySelector('.order-number').innerHTML=order
+    }
+    
 }
 
 const meals=createCatagories(catagories);
